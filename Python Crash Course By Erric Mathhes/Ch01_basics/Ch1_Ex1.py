56131ce8a6cd0e91d93af67ac89358658b8b7e8a
Muhamad  Hamza Hanif <44979074+mHamzaHanif@users.noreply.github.com>
2019-07-31 23:42:50 +0500

    Add files via upload

---
+++ Python Crash Course By Erric Mathhes/Ch01_basics/Ch1_Ex1.py
@@ -0,0 +1,14 @@
+# ***********************Ch1_Ex1: Print these lines*****************
+# this is double \\ backslash
+#these are mountains /\/\/\/\ /\
+# he is        awesome(use escape sequence)
+
+print("this is double \\\\ backslash")
+print("these are mountains /\\/\\/\\/\\/\\")
+print("he is\t\t awesome")
+print(" \\\" \\\' \\t  \\n") 
+
+print(r" \"  \'  \t  \n") #for raw input
+
+#  emoji site:      https://unicode.org/emoji/charts/full-emoji-list.html
+print("\U0001F970") # this print emoji, U+1F970 -> replace '+' with three 0's & place '\' in the biggining
